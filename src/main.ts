dce6977947dbcc5fe7979e2e6ac9a42be0fecbf3
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-03-28 10:25:23 +0200

    U - Actualización para componentes standalone y configuración de rutas e interceptores

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,23 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { Routes, provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { httpInterceptor } from './app/shared/interceptors/http.interceptor';
 
-import { AppModule } from './app/app.module';
+const routes: Routes = [
+  { path: '', redirectTo: 'form', pathMatch: 'full' },
+  {
+    path: 'form',
+    loadComponent: () =>
+      import('./app/components/form/form.component').then(
+        (m) => m.FormComponent
+      ),
+  },
+];
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(withInterceptors([httpInterceptor])),
+    provideRouter(routes),
+  ],
+}).catch((err) => console.error(err));
